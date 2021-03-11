15bae18a06baa0c83d0389034800d4eb7e5c7488
mhhasanmehedi <mmmehedi53@gmail.com>
2021-03-11 16:41:59 +0600

    readme edit

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="position-relative text-center text-light">
             <img src={banner} className="banner-img" alt=""/>
-            <h1 className="position-absolute" style={{transform:'translate(-50%, -50%)',top:'50%',left:'50%'}}>Soccer Team Tracker</h1>
+            <h1 className="position-absolute" style={{transform:'translate(-50%, -50%)',top:'50%',left:'50%'}}>Soccer Teams</h1>
         </div>
     );
 };
